fc6a6b1dc9216b4b0524c5fce5796c265afeac35
souma1024 <souma3789522@gmail.com>
2025-10-19 18:25:28 +0900

    add button to insert sample data

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:original_dict_app/widgets/common_drawer.dart';
 import 'package:original_dict_app/widgets/search_box.dart';
 import 'package:original_dict_app/screens/home/words_list_screen.dart';
+import 'package:original_dict_app/utils/test/insert_sample_data.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -19,11 +20,17 @@ class HomeScreen extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
           child: Column(
-            children: const [
+            children:  [
               SearchBox(),
               SizedBox(height: 16),
               // 検索結果リスト（残りの高さを全部使う）
               Expanded(child: WordListScreen()),
+              ElevatedButton(  //これはテスト用、本番はボタンごと消す。
+                onPressed: () async {
+                  await insertSampleData();
+                },
+                child: Text('サンプルデータ投入'),
+              ),
             ],
           ),
         ),
