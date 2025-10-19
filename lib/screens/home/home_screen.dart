87a31f013a832070340919ce64ca77fa564e2a52
souma1024 <souma3789522@gmail.com>
2025-10-19 15:39:05 +0900

    divide main.dart into home_screen.dart

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:original_dict_app/widgets/common_drawer.dart';
+import 'package:original_dict_app/widgets/search_box.dart';
+import 'package:original_dict_app/screens/home/words_list_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('オリジナル辞書アプリ'),
+      ),
+      drawer:  CommonDrawer(),
+      // SafeAreaを使うと、どんな端末でも安全にUIが表示される
+      // 時刻などが書いてある画面最上部にはUIを設置しないようにするなど
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
+          child: Column(
+            children: const [
+              SearchBox(),
+              SizedBox(height: 16),
+              // 検索結果リスト（残りの高さを全部使う）
+              Expanded(child: WordListScreen()),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Navigator.pushNamed(context, '/add');
+          // 今は仮
+          //debugPrint('押したね？');
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
