4dfb0da4ddabfba439ec09a9f4c9fca454eb20e9
souma1024 <souma3789522@gmail.com>
2025-10-25 19:35:33 +0900

    modify a bit

--- lib/dto/card_preview.dart
+++ lib/dto/card_preview.dart
@@ -1,22 +1,25 @@
+import 'package:original_dict_app/utils/db/time_helper.dart';
+import 'package:original_dict_app/utils/db/text_length_helper.dart';
+
 class CardPreview {
   final int id;
   final String name;
   final String intro;
-  final DateTime updatedAt;
+  final String updatedAtText;
 
   CardPreview({
     required this.id,
     required this.name,
     required this.intro,
-    required this.updatedAt,
+    required this.updatedAtText,
   });
 
   factory CardPreview.fromMap(Map<String, dynamic> m) {
     return CardPreview(
       id: m['id'] as int,
       name: m['name'] as String,
-      intro: m['intro'] as String,
-      updatedAt: DateTime.parse(m['updated_at'] as String),
+      intro: limitTextLength(m['intro'] as String),
+      updatedAtText: TimeHelper.formatDateTime(DateTime.parse(m['updated_at'] as String))
     );
   }
 }
